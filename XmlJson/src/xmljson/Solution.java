a89800165c8900d75ad1bd8c70b83eba8e23bdcb
bino <ska3287776@naver.com>
2023-07-31 19:52:30 +0900

    update kopo-java

--- XmlJson/src/xmljson/Solution.java
+++ XmlJson/src/xmljson/Solution.java
@@ -11,91 +11,16 @@ public class Solution {
 
 	public static void main(String[] args) {
 		
-//		String[] babbling = {"aya", "yee", "u", "maa", "wyeoo"};
-//		String[] babbling = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
-		
-//		System.out.println(solution(babbling));
-		
-//		String[] babbling = {"aya", "yee", "u", "maa"};
-//		String[] babbling = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
-//		
-//		System.out.println(solutions(babbling));
-		
-		Map<String, List<String>> map = new HashMap<String, List<String>>();
-		List<String> list = new ArrayList<String>();
-		list.add("brazil");
-		map.put("nation", list);
-		list.add("176cm");
-		map.put("height", list);
-		
-		Set<String> playersByFilter = map.keySet();
-		playersByFilter.;
-		
-		System.out.println(playersByFilter.size());
-		
-		String key = "";
-		
-		for (String key1 : map.keySet()) {
-			
-		}
-//		String height = map.get("nation=brazil").get(0);
-		System.out.println(map.get("height"));
+		System.out.println(solution(2500, 2500, 1));
 	}
 	
-		public static int solution1(String[] babbling) {
-			int answer = 0;
-				String[] ongal = {"aya", "ye", "woo", "ma"};
-		
-				for(int i = 0; i < babbling.length; i++) {
-					for(int j = 0; j < ongal.length; j++) {
-						if(babbling[i].contains(ongal[j]) && !(babbling[i].contains(ongal[j] + ongal[j]))) {
-							babbling[i] = babbling[i].replace(ongal[j], " ").trim();
-						}
-					}
-					System.out.println("원소2 : " + babbling[i]);
-					if(babbling[i].equals("")) answer++;
-				}
-					
-				return answer;
-	   }
-	
-	    public static int solution(String[] babbling) {
-	    	int answer = 0;
-	    	int a = 0; int b = 0;
-			String[] ongal = {"aya", "ye", "woo", "ma"};
-
-			for(int i = 0; i < babbling.length; i++) {
-				for(int j = 0; j < ongal.length; j++) {                
-					if(babbling[i].contains(ongal[j])) {
-						babbling[i] = babbling[i].replace(ongal[j], "1").trim();
-					}
-				}
-				if(babbling[i].contains("1")) a++; // 1 포함하면 a 증가
-				
-				// 문자 포함하면 b 증가
-				char[] arr = new char[26];
-				
-				for (int k = 0; k < arr.length; k++) {
-					arr[k] = (char)(97 + k);
-				}
-				
-				for (int k = 0; k < arr.length; k++) {
-					if(babbling[i].contains(String.valueOf(arr[k]))) {
-						b++;
-					}
-//					System.out.println(arr[k]);
-				}
-					
-				answer = a - b;
-				
-				System.out.println("원소 : " + babbling[i]);
-//				System.out.println("a : " + a);
-//				System.out.println("b : " + b);
-				if(babbling[i].equals("")) answer++;
-			}
-			
-			return answer;
-	    }
+	public static long solution(int price, long money, int count) {
+        long a = 0;
+        for(int i = 1; i <= count; i++) {
+            a += price * i;
+        }
+        return a - money;
+    }
 	    
 	    
 	    public static int solutions(String[] babbling) {
